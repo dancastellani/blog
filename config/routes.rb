247c4cdd252853fa79a9158bd049c4915e605a95
Daniel Castellani <dancastellani@gmail.com>
2013-07-22 06:17:28 +0000

    added comments to posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Blog::Application.routes.draw do
   
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
  
   root to: "welcome#index"
   # The priority is based upon order of creation: first created -> highest priority.
