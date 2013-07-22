85cc6cc5fc406e103280b6a692a105c0fc3b28c1
Daniel Castellani <dancastellani@gmail.com>
2013-07-22 06:01:45 +0000

    created the blog with Post CRUD

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '6176e3f59e08303dd61c74e085d8d3bffff9ef7b8ca7e5ae94ee0d3660c1e11f0f3da061b5bbd4d45c80fa9afc31001cada0cb16fa9ddf19a31915a7ccadf400'
